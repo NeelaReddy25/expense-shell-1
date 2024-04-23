9a89670b1947d0978b2af2aff7acfcec89416a9b
NeelaReddy <neelareddy.i25@gmail.com>
2024-04-23 23:07:21 +0530

    some-message

--- mysql-1.sh
+++ mysql-1.sh
@@ -8,23 +8,14 @@ echo "Please enter DB password:"
 read mysql_root_password
 
 dnf install mysql-server -y &>>$LOGFILE
-#VALIDATE $? "Installing MYSQL server"
-
 systemctl enable mysqld &>>$LOGFILE
-#VALIDATE $? "Enabling MYSQL"
-
 systemctl start mysqld &>>$LOGFILE
-#VALIDATE $? "Starting MYSQL"
-
-# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-# VALIADTE $? "Setting up root password"
 
 #Below code will be useful for idempotent secure
 mysql -h db.neelareddy.store -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-    #VALIDATE $? "MYSQL Root password setup"
 else
     echo -e "MYSQL Root password setup...$Y SKIPPING $N"
 fi    
