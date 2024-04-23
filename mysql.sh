230c14efe3a8bb277c7bca05b3255fcbae583580
NeelaReddy <neelareddy.i25@gmail.com>
2024-04-23 19:58:10 +0530

    Do good and take better

--- mysql.sh
+++ mysql.sh
@@ -14,14 +14,14 @@ systemctl enable mysqld &>>$LOGFILE
 VALIDATE $? "Enabling MYSQL"
 
 systemctl start mysqld &>>$LOGFILE
-VALIADTE $? "Starting MYSQL"
+VALIDATE $? "Starting MYSQL"
 
 # mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
 # VALIADTE $? "Setting up root password"
 
 #Below code will be useful for idempotent secure
 mysql -h db.neelareddy.store -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
-if [$? -ne 0 ]
+if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
 else
